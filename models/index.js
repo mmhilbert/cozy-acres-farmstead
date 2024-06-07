d8a460567bba5c4a3f83bb773475b603cfb2ea97
Megan Hilbert <meganmhilbert@gmail.com>
2024-06-06 19:23:20 -0500

    model associations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,36 @@
 const User = require('./User');
+const Farm = require('./Farm')
+const FarmAnimal = require('./FarmAnimal')
+const Animal = require('./Animal')
 
-module.exports = { User };
+
+
+User.hasOne(Farm, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Farm.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Farm.hasMany(FarmAnimal, {
+    foreignKey: 'farm_id'
+})
+
+FarmAnimal.belongsTo(Farm, {
+    foreignKey: 'farm_id'
+})
+
+Animal.hasMany(FarmAnimal, {
+    foreignKey: 'animal_id',
+    onDelete: 'CASCADE'
+})
+
+FarmAnimal.belongsTo(Animal, {
+    foreignKey: 'animal_id',
+})
+
+
+
+module.exports = { User, Farm, FarmAnimal, Animal };
